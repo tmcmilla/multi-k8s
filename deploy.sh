af4595e1a93786559213324a3cd2ba79db00a467
tmcmilla <thomasc.mcmillan@gmail.com>
2020-01-03 15:50:05 -0800

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t tmcmilla/multi-client:latest -t tmcmilla/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t tmcmilla/multi-server:latest -t tmcmilla/multi-server:$SHA -f ./server/Dockerfile ./server
-docker bulld -t tmcmillla/multi-worker:latest -t tmcmilla/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker bulld -t tmcmilla/multi-worker:latest -t tmcmilla/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push tmcmilla/multi-client:latest
 docker push tmcmilla/multi-server:latest
 docker push tmcmilla/multi-worker:latest
@@ -11,5 +11,5 @@ docker push tmcmilla/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=tmcmilla/multi-server:$SHA
-kubectl set image deployments/client-deployment server=tmcmilla/multi-client:$SHA
+kubectl set image deployments/client-deployment client=tmcmilla/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=tmcmilla/multi-worker:$SHA
